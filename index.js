7f6bcd7bc01073085d156460e4446c9681f1ae70
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-04-04 15:52:49 -0700

    Frontend is communicating with backend

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const path = require("path")
 
 const server = express()
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
-
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html' ))
+})
 
 const PORT = process.env.PORT || 3200
 
