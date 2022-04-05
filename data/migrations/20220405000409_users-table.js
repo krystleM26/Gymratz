66e0d16e8619830771760f5fe687e29ac1dcf55a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-04-04 20:07:13 -0700

    Migrations are complete

--- data/migrations/20220405000409_users-table.js
+++ data/migrations/20220405000409_users-table.js
@@ -1,15 +1,47 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments()
+      tbl.text('name', 100).notNullable()
+      tbl.string('username', 125).unique(),notNullable()
+      tbl.string('password', 125).unique().notNullable()
+      tbl.string('type', 100).notNullable()
+
+      
   
+    
+  })
+  .createTable('classes', tbl => {
+      tbl.increments()
+      tbl.date('date').notNullable()
+      tbl.time('time').notNullable()
+      tbl.integer('duration').notNullable()
+      tbl.string('type', 100).notNullable()
+      tbl.string('intensity', 20).notNullable()
+      tbl.string('location', 125).notNullable()
+   
+
+
+  })
+  .createTable('instructors', tbl => {
+      tbl.increments()
+      tbl.string('name', 125).notNullable()
+      tbl.string('start-time', 100).notNullable()
+      tbl.integer('duration').notNullable()
+      tbl.integer('attendees', 100).notNullable()
+      tbl.string('intensity', 20).notNullable()
+      tbl.string('location', 125).notNullable()
+      tbl.integer('maximum', 30).notNullable()
+  })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+
+.dropTableIfExists('users')
+.dropTableIfExists('instructors')
+.dropTableIfExists('classes')
+
+}
