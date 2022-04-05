6057029661738502da02fdb51bbedd82bfe140db
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-04-04 21:23:55 -0700

    Users db access complete

--- backend/db-model/users-model.js
+++ backend/db-model/users-model.js
@@ -1,5 +1,36 @@
 const db = require('../../data/dbConfig')
 
-function findby(username) {
+function find(username) {
     return db('users').where('username', username).first()
+}
+
+function findBy(filter) {
+    return db('users').where(filter)
+}
+
+function get() {
+    return db('users')
+}
+
+function getById(id) {
+    return db('users').where('id', id).first()
+
+}
+
+async function insert(user) {
+    cond [id] = await db('users').insert(user)
+}
+
+async function update(id, changes) {
+    await db('users').update(changes).where('id', id)
+    return getById(id)
+}
+
+module.exports = {
+    find,
+    getById,
+    findBy,
+    get,
+    getById,
+    update,
 }
